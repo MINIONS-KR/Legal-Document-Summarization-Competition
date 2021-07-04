e5b5d9c8b2dd888ca1e90cdd3941eee51487ebaf
arabae <arbae@inu.ac.kr>
2021-07-04 15:27:21 +0900

    add ARA models in shell

--- inference.sh
+++ inference.sh
@@ -1,4 +1,10 @@
 python3 ./IKHYO/predict.py
 python3 ./BERTSUM/predict.py
-
-# python3 ./ensemble.py
+python3 ./ARA/predict.py --model_name kobert0
+python3 ./ARA/predict.py --model_name kobert1
+python3 ./ARA/predict.py --model_name kobert2
+python3 ./ARA/predict.py --model_name kobert3
+python3 ./ARA/predict.py --model_name kobert4
+python3 ./ARA/predict.py --model_name koelectra
+python3 ./ARA/predict.py --model_name sentavg
+python3 ./ensemble.py
--- train.sh
+++ train.sh
@@ -1,2 +1,6 @@
 python3 ./IKHYO/train.py
 python3 ./BERTSUM/train.py
+python3 ./ARA/data_split.py
+python3 ./ARA/kfold_train.py
+python3 ./ARA/train.py --model_name koelectra
+python3 ./ARA/train.py --model_name sentavg
